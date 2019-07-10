662a8ac7ec41b81fede2a36c90594d2dffd93ad7
askmebefore <zayats1812@mail.ru>
2019-07-10 18:22:34 +0300

    faster implemntation of pairwise R2: trash removed, pairwise squared diff is now done via pre-calculated matrix multiplication

--- src/pairwiseR2.cpp
+++ src/pairwiseR2.cpp
@@ -13,31 +13,22 @@ arma::mat pairwiseR2(const arma::mat& X) {
   int genes_count = X.n_cols;
   int sample_count = X.n_rows;
   
+  mat Y = square(X);
+  rowvec Ysum = sum(Y);
+  mat Z = 2 * X.t() * X;
+  
   mat r2(genes_count, genes_count, fill::zeros);
   vec tsss(genes_count);
   
-  vec sxx(genes_count);
-  
   for (int i = 0; i < genes_count; i++) {
-    sxx[i] = dot(X.col(i), X.col(i));
-    
-    vec x_int(sample_count);
     double mean_x_intercept = mean(X.col(i));
-    for (int j = 0; j < sample_count; j++) {
-      x_int[j] = X.at(j, i) - mean_x_intercept;
-    }
+    vec x_int = X.col(i) - mean_x_intercept;
     double tss = dot(x_int, x_int);
     tsss[i] = tss;
   }
   
   for (int i = 0; i < genes_count; i++) {
-    rowvec sxy = X.col(i).t() * X;
-    
-    vec rss(genes_count);
-    for (int j = 0; j < genes_count; j++) {
-      vec z = X.col(j) - X.col(i);
-      rss[j] = dot(z, z);
-    }
+    vec rss = Ysum[i] + (Ysum - Z.row(i)).t();
     r2.col(i) += (ones(genes_count) - rss / tsss) / 2.0;
     r2.row(i) += ((ones(genes_count) - rss / tsss) / 2.0).t();
   }
